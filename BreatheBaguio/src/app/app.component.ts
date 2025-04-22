b907c6f09992ba1f84ade6fcb72beb347c8a374a
UBSITAlhambra <20124561@s.ubaguio.edu>
2025-04-22 16:21:50 +0800

    Temporarily done with the contact form. Needed fixing in the html and the its component.ts

--- BreatheBaguio/src/app/app.component.ts
+++ BreatheBaguio/src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './about/contact/contact.component';
 import { HomeComponent } from './home/home.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, AboutComponent, ContactComponent, HomeComponent],
+  imports: [RouterOutlet, AboutComponent, ContactComponent, HomeComponent, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
