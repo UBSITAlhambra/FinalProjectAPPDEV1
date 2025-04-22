5bb9449fd942aca5e3235095e9b4809e55320818
UBSITAlhambra <20124561@s.ubaguio.edu>
2025-04-22 16:03:43 +0800

    temporary commit for links to be able to view about and contact page

--- BreatheBaguio/src/app/app.routes.ts
+++ BreatheBaguio/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './about/contact/contact.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/home', pathMatch: 'full' }, 
+    { path: 'home', component: HomeComponent },  
+    { path: 'about', component: AboutComponent },
+    { path: 'about/contact', component: ContactComponent }
+];
