2a3f229341766d8d9e4006bbaf9fbc58e696a937
UBSITCerezo <20234032@s.ubaguio.edu>
2025-04-22 14:49:50 +0800

    creation of the component map and quiz page

---
+++ BreatheBaguio/src/app/quiz-page/quiz-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-quiz-page',
+  imports: [],
+  templateUrl: './quiz-page.component.html',
+  styleUrl: './quiz-page.component.css'
+})
+export class QuizPageComponent {
+
+}
